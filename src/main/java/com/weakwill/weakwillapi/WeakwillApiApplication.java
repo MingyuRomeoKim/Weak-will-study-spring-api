cc2d08d2048e240181602d5f8eee90e453c484f4
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-02-04 21:57:50 +0900

    프로젝트 개설

---
+++ src/main/java/com/weakwill/weakwillapi/WeakwillApiApplication.java
@@ -0,0 +1,13 @@
+package com.weakwill.weakwillapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeakwillApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeakwillApiApplication.class, args);
+    }
+
+}
