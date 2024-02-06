08bec5caede40ba410f535fd7d748d600004a250
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-02-06 13:11:29 +0900

    test jenkins file5

--- src/main/java/com/weakwill/weakwillapi/controllers/HelloController.java
+++ src/main/java/com/weakwill/weakwillapi/controllers/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "hello world!!! 이제부터 Spring Boot를 사용해서 API를 만들어 보자고!! ";
+        return "hello world!!! 이제부터 Spring Boot를 사용해서 API를 만들어 보자고!! 아직 테스트중. ";
     }
 }
